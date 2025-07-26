49ca1f943f7f5a47854f39760b430bb3c4c248db
Vricap <vricagedepenggalih@gmail.com>
2025-07-26 17:40:48 +0700

    render pdf file from the server response

--- src/pages/Sent.jsx
+++ src/pages/Sent.jsx
@@ -1,18 +1,56 @@
 import { useLocation } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import axios from "axios";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
 
-function Sent() {
+export default function Sent() {
+  const [blobUrl, setblobUrl] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [pageDims, setPageDims] = useState({ width: 0, height: 0 });
   const [pointerPos, setPointerPos] = useState(null);
 
   const containerRef = useRef();
+  const location = useLocation();
+  const { id, title } = location.state || {};
+
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}/documents/blob/${id}`,
+          {
+            headers: {
+              "x-access-token": localStorage.getItem("token"),
+            },
+            responseType: "blob", // Tell axios to treat the response as a Blob
+          },
+        );
+        setblobUrl(URL.createObjectURL(response.data));
+      } catch (err) {
+        // TODO: since we tell axios to always treat all response as blob, if server response an json error, we cant read the error message
+        setError(`Failed to load documents. ${err}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDocuments();
+  }, []);
+
+  if (loading) {
+    return <p className="p-4">Loading documents...</p>;
+  }
+
+  if (error) {
+    return <p className="p-4 text-red-500">{error}</p>;
+  }
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -49,8 +87,6 @@ function Sent() {
     setPointerPos(pos);
     // onSave(pos); // send to backend or store in parent
   }
-  const location = useLocation();
-  const { id, title } = location.state || {};
 
   return (
     <main className="container mx-auto py-6 px-4 md:px-6">
@@ -77,7 +113,7 @@ function Sent() {
             onDrop={handleDrop}
             style={{ minHeight: 400 }}
           >
-            <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+            <Document file={blobUrl} onLoadSuccess={onDocumentLoadSuccess}>
               <Page
                 pageNumber={pageNumber}
                 onRenderSuccess={onPageRenderSuccess}
@@ -124,4 +160,3 @@ function Sent() {
     </main>
   );
 }
-export default Sent;
