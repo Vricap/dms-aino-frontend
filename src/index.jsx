98b87d93b237a056fc548a290e54cfe2d5cca39b
Vricap <vricagedepenggalih@gmail.com>
2025-11-10 19:54:10 +0700

    the local pdf worker build is now work perfectly in dev & prod

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/globals.css";
 import { pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.mjs`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
