be18c8e9051ef5d08929cbbb102c7a870572eb1c
Vricap <vricagedepenggalih@gmail.com>
2025-12-07 22:08:04 +0700

    feat: add dashboard & graph

---
+++ src/components/bar-chart.jsx
@@ -0,0 +1,60 @@
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+export default function BarChart({ values }) {
+  const colors = [
+    "rgba(239, 68, 68, .7)", // red-500
+    "rgba(59, 130, 246, .7)", // blue-500
+    "rgba(16, 185, 129, .7)", // green-500
+    "rgba(245, 158, 11, .7)", // yellow-500
+    "rgba(139, 92, 246, .7)", // purple-500
+  ];
+
+  const data = {
+    labels: ["Mengupload", "Mengirim", "Inbox", "Complete", "Menanda Tangani"],
+    datasets: [
+      {
+        label: "Documents",
+        data: [
+          values.uploaded ?? 0,
+          values.sended ?? 0,
+          values.inbox ?? 0,
+          values.complete ?? 0,
+          values.signed ?? 0,
+        ],
+        borderColor: "#d3dceb",
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        ticks: {
+          stepSize: 1, // only whole numbers
+          precision: 0, // no decimals
+        },
+        beginAtZero: true,
+      },
+    },
+    plugins: { legend: { display: false } },
+  };
+
+  return (
+    <div className="w-full h-96 bg-background rounded shadow p-4">
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,50 @@
-import { DashboardStats } from "../components/dashboard-stats";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Send, Inbox, FileSignature, CheckCircle, Upload } from "lucide-react";
+import BarChart from "../components/bar-chart.jsx";
 
 export default function Dashboard() {
+  const [dashboard, setDashboard] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchDocuments = async (period) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/documents/dashboard/?period=${period}`,
+        {
+          headers: {
+            "x-access-token": localStorage.getItem("token"),
+          },
+        },
+      );
+      setDashboard(response.data.data);
+    } catch (err) {
+      setError(`Gagal dalam load dashboard. ${err.response?.data?.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDocuments("all");
+  }, []);
+
+  if (loading) {
+    return <p className="p-4">Loading documents...</p>;
+  }
+
+  if (error) {
+    return <p className="p-4 text-red-500">{error}</p>;
+  }
+
   return (
     <main className="container mx-auto py-6 px-4 md:px-6">
       <div className="flex flex-col gap-6">
@@ -13,7 +57,115 @@ export default function Dashboard() {
           </p>
         </div>
 
-        <DashboardStats />
+        <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-5">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <div className="flex flex-row items-center justify-between w-full">
+                <CardTitle className="text-sm font-medium">
+                  Dokumen Ter-upload
+                </CardTitle>
+                <Upload className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{dashboard.uploaded}</div>
+              {/* <p className="text-xs text-muted-foreground">
+                +12 from last month
+              </p>*/}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <div className="flex flex-row items-center justify-between w-full">
+                <CardTitle className="text-sm font-medium">
+                  Dokumen Dikirim
+                </CardTitle>
+                <Send className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{dashboard.sended}</div>
+              {/* <p className="text-xs text-muted-foreground">
+                +8 from last month
+              </p>*/}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <div className="flex flex-row items-center justify-between w-full">
+                <CardTitle className="text-sm font-medium">
+                  Dokumen Inbox
+                </CardTitle>
+                <Inbox className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{dashboard.inbox}</div>
+              {/* <p className="text-xs text-muted-foreground">
+                +8 from last month
+              </p>*/}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <div className="flex flex-row items-center justify-between w-full">
+                <CardTitle className="text-sm font-medium">
+                  Dokumen Complete
+                </CardTitle>
+                <CheckCircle className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{dashboard.complete}</div>
+              {/* <p className="text-xs text-muted-foreground">+3 from last week</p>*/}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <div className="flex flex-row items-center justify-between w-full">
+                <CardTitle className="text-sm font-medium">
+                  Telah Menanda Tangani
+                </CardTitle>
+                <FileSignature className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{dashboard.signed}</div>
+              {/* <p className="text-xs text-muted-foreground">+5 from last week</p>*/}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+      <Button className="mt-4 text-left p-1" variant="outline">
+        <select className="bg-background w-full h-full p-2">
+          <option key="all" value="all" onClick={() => fetchDocuments("all")}>
+            Semua
+          </option>
+          <option
+            key="daily"
+            value="daily"
+            onClick={() => fetchDocuments("daily")}
+          >
+            Hari Ini
+          </option>
+          <option
+            key="weekly"
+            value="weekly"
+            onClick={() => fetchDocuments("weekly")}
+          >
+            Minggu Ini
+          </option>
+          <option
+            key="monthly"
+            value="monthly"
+            onClick={() => fetchDocuments("monthly")}
+          >
+            Bulan Ini
+          </option>
+        </select>
+      </Button>
+      <div className="p-4">
+        <BarChart values={dashboard} />
       </div>
     </main>
   );
